4b2a16267d3af0aaa3ada8974a641612744010b1
M1saka2003 <3296582213@qq.com>
2023-12-22 15:23:57 +0800

    Server

---
+++ Server/cmake-build-debug/Currency/CMakeFiles/Currency.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Currency.dir/Buffer.cpp.o"
+  "CMakeFiles/Currency.dir/Buffer.cpp.o.d"
+  "CMakeFiles/Currency.dir/Channel.cpp.o"
+  "CMakeFiles/Currency.dir/Channel.cpp.o.d"
+  "libCurrency.a"
+  "libCurrency.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Currency.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Server/cmake-build-debug/Tcp/CMakeFiles/Tcp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tcp.dir/TcpConnection.cpp.o"
+  "CMakeFiles/Tcp.dir/TcpConnection.cpp.o.d"
+  "CMakeFiles/Tcp.dir/TcpServer.cpp.o"
+  "CMakeFiles/Tcp.dir/TcpServer.cpp.o.d"
+  "libTcp.a"
+  "libTcp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tcp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
